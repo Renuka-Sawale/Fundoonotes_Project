f2b680c434b64ab04ff093708c04f91a9997566a
Renuka-Sawale <rrsuryawanshi11@gmail.com>
2021-06-29 21:52:18 +0530

    Added registration ui with button and checkbox

--- src/Pages/RegistrationPage/RegistrationPage.jsx
+++ src/Pages/RegistrationPage/RegistrationPage.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
 import '../RegistrationPage/registration.css'
 
 class RegistrationPage extends Component {
@@ -7,10 +9,7 @@ class RegistrationPage extends Component {
         return (
             <div class="content">
                 <form class="top-content">
-                    {/* <div>
-                        <img src="https://ssl.gstatic.com/accounts/signup/glif/account.svg" alt="" width="24" height="24" class="j9NuTc TrZEUc"/> 
-
-                    </div> */}
+                   
                    <div className="fundooo">
                         <h1 style={{color : '#34a853'}}>F</h1>
                         <h1 style={{color : '#4285f4'}}>u</h1>
@@ -34,6 +33,19 @@ class RegistrationPage extends Component {
                         <TextField id="outlined-basic" label="Password" variant="outlined" />
                         <TextField id="outlined-basic" label="ConfirmPassword" variant="outlined" />
                     </div>
+                    <div class= "checkbox">
+                        <Checkbox defaultChecked color="primary" inputProps={{ 'aria-label': 'secondary checkbox' }} />
+                        {/* <p>Show Password</p> */}
+                        
+                    </div>
+                    <div class="Bottom-Button">
+                        <Button variant="contained" color="primary" href="#contained-buttons">
+                            Next
+                        </Button>
+                    </div>
+                    {/* <div>
+                        <img src="https://ssl.gstatic.com/accounts/signup/glif/account.svg" alt="" width="100" height="100" class="j9NuTc TrZEUc"/> 
+                    </div> */}
                 </form>
             </div>
         )
